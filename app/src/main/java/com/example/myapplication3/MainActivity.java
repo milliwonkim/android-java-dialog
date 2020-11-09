5b3265adab38e4ea23252c56ace073d9663dba9b
milliwonkim <rldnjs9347@gmail.com>
2020-11-10 00:09:14 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements ExampleDialog.ExampleDialogListener {
+
+    private TextView textViewUsername;
+    private TextView textViewPassword;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewUsername = (TextView) findViewById(R.id.textview_username);
+        textViewPassword = (TextView) findViewById(R.id.textview_password);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+
+        });
+    }
+    public void openDialog() {
+        ExampleDialog exampleDialog = new ExampleDialog();
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
+    }
+
+    @Override
+    public void applyTexts(String username, String password) {
+        textViewUsername.setText(username);
+        textViewPassword.setText(password);
+    }
+}
